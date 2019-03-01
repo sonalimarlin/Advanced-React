7a07965b8e876908c0a3a4af960ecbc272827780
sonalimarlin <sonali.marlin@gmail.com>
2019-02-28 21:53:12 -0700

    add nprogress and _document to pre-load css

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,21 @@
-import Nav from './Nav';
 import Link from 'next/link';
 import styled from 'styled-components';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Nav from './Nav';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+    console.log('sdsd');
+    
+  };
+
+  Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+Router.onRouteChangeError = () => {
+    NProgress.done();
+};
 
 const Logo = styled.h1`
     font-size: 4rem;
